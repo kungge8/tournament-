874307200364a4fbb1fe9fb9f9401133e3d56ad5
Charles Dunn <charlesdunn@Charless-MacBook-Pro.local>
2017-10-16 13:23:37 -0400

    html routes

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,22 +2,22 @@ var path = require("path");
 
 module.exports = function (app){
 
-//we need tl add the html page names once we have them completed
+//we need to add the html page names once we have them completed
 	app.get("/", function (req,res){
 
-		res.sendFile(path.join(__dirname, "../public/"));
+		res.sendFile(path.join(__dirname, "../public/home.handlebars"));
 
 	});
 
 	app.get("/search", function (req,res){
 
-		res.sendFile(path.join(__dirname, "../public/"));
+		res.sendFile(path.join(__dirname, "../public/search.handlebars"));
 
 	});
 
 	app.get("/create", function (req,res){
 
-		res.sendFile(path.join(__dirname, "../public/"));
+		res.sendFile(path.join(__dirname, "../public/create.handlebars"));
 
 	});
 
